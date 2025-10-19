2f8c37495fa7e7f3df4e07059eb2423dee8a0f72
hafifradhitya <bangclasher81@gmail.com>
2025-10-19 11:46:42 +0700

    first commit

---
+++ src/app/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='text-sm text-center italic'>
+        <p>&copy; 2025 Foresight. Ramalan ini berdasarkan model AI vision, bukan untuk dipercaya sepenuhnya</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/app/components/Navheader.js
@@ -0,0 +1,10 @@
+function Navheader() {
+  return (
+    <header className="text-center">
+      <h1 className="text-4xl font-extrabold bg-gradient-to-r from-green-300 via-orange-600 to-cyan-400 text-transparent bg-clip-text shadow-lg inline-block" style={{ filter: 'drop-shadow(0 0 10px rgba(23, 70, 150, 0.8))' }}>MYFACE | PREDICT YOUR FACE</h1>
+      <p className="text-gray-300 italic">Ramal berdasarkan foto selfie kalian</p>
+    </header>
+  )
+}
+
+export default Navheader
